83ecf1e6b5503a4f045e7c956f391c59cc5c4065
Nikita Verba <nikita.verba18052004@gmail.com>
2025-06-22 11:04:26 +0300

    Added header to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,43 @@
 "use client"
 
-import { useEffect } from "react"
-import { supabase } from "@/lib/supabaseClient"
 import { useRouter } from "next/navigation"
+import Link from "next/link"
+import { useAuth } from "@/contexts/AuthContext"
 
 export default function HomePage() {
-  const router = useRouter()
-
-  useEffect(() => {
-    // Handle the authentication callback
-    const handleAuthCallback = async () => {
-      const { data: { session } } = await supabase.auth.getSession()
-      
-      if (session) {
-        router.push("/dashboard")
-      }
-    }
-
-    handleAuthCallback()
-  }, [router])
+  const { user, session } = useAuth()
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-6">
-      <h1 className="text-2xl font-bold mb-4">Welcome to Posty</h1>
-      <p className="mb-4">Please login to continue</p>
-      <a href="/login" className="bg-black text-white rounded p-3">
-        Go to Login
-      </a>
+    <div>
+      <nav className="navbar rounded-box shadow-base-300/20 shadow-sm">
+            <div className="w-full md:flex md:items-center md:gap-2">
+                <div className="flex items-center justify-between">
+                <div className="navbar-start items-center justify-between max-md:w-full">
+                    <Link 
+                      href="/dashboard" 
+                      className="link text-base-content link-neutral text-xl font-bold no-underline"
+                    >
+                      Posty
+                    </Link>
+                    <div className="md:hidden">
+                    <button type="button" className="collapse-toggle btn btn-outline btn-secondary btn-sm btn-square" data-collapse="#default-navbar-collapse" aria-controls="default-navbar-collapse" aria-label="Toggle navigation" >
+                        <span className="icon-[tabler--menu-2] collapse-open:hidden size-4"></span>
+                        <span className="icon-[tabler--x] collapse-open:block hidden size-4"></span>
+                    </button>
+                    </div>
+                </div>
+                </div>
+                <div id="default-navbar-collapse" className="md:navbar-end collapse hidden grow basis-full overflow-hidden transition-[height] duration-300 max-md:w-full" >
+                <ul className="menu md:menu-horizontal gap-2 p-0 text-base max-md:mt-2">
+                    {user && session ? (
+                      <li><Link href="/dashboard">Dashboard</Link></li>
+                    ) : (
+                      <li><Link href="/login">Login</Link></li>
+                    )}
+                </ul>
+              </div>
+          </div>
+        </nav>
     </div>
   )
 }
